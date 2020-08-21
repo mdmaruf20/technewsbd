73939120e97208cc85c55d0ff45e774a9aedebea
mdmaruf20 <68922408+mdmaruf20@users.noreply.github.com>
2020-08-21 21:14:06 +0600

    Update wp-config-sample.php
    agerta

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,8 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-$db = parse_url(getenv("DATABASE_URL"));
-$db["path"] = ltrim($db["path"], "/");
+define( 'DB_NAME', 'database_name_here' );
+
+/** MySQL database username */
+define( 'DB_USER', 'username_here' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'password_here' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
 
 
 /** Database Charset to use in creating database tables. */
